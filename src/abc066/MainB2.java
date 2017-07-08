e5b49ea6dad8312cf5c50ef09caf8de81e3878ed
0x306e <3so.d.na@gmail.com>
2017-07-09 01:36:16 +0900

    　

--- src/abc066/MainB2.java
+++ src/abc066/MainB2.java
@@ -1,20 +1,21 @@
 package abc066;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MainB {
+public class MainB2 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String str = br.readLine();
 
     for (int i = 0; i < str.length(); i++) {
-      String s = str.substring(0, i + 2);
+      String s = str.substring(0, str.length() - (1 + i));
 
       System.out.println(s);
-      if (str.indexOf(s) == str.lastIndexOf(s)) {
-        System.out.println(s.length());
+      if (str.indexOf(s) != str.lastIndexOf(s)) {
+        System.out.println(s.length() + 1);
         break;
       }
 
