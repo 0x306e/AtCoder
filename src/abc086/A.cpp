d78675cdce71fbe9fc0b680b9d5b30b1fcfe358c
0x306e <3so.d.na@gmail.com>
2018-03-30 01:51:27 +0900

    C RE

---
+++ src/abc086/A.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+int main(int argc, char* argv[]){
+    int a, b;
+    cin >> a >> b;
+    if(a%2==0 || b%2==0) cout << "Even";
+    else cout << "Odd";
+}
