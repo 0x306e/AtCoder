5f88763b7597a08d86d3d59439510cfaca2e89b6
0x306e <3so.d.na@gmail.com>
2018-07-02 10:21:52 +0900

    AC

--- src/abc094/D.cpp
+++ src/abc094/D.cpp
@@ -1,34 +1,27 @@
+#define _overload3(_1,_2,_3,name,...) name
+#define _rep(i,n) repi(i,0,n)
+#define repi(i,a,b) for(int i=int(a);i<int(b);++i)
+#define rep(...) _overload3(__VA_ARGS__,repi,_rep,)(__VA_ARGS__)
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
 
 int main(int argc, char* argv[]){
-    int n, tmp, max = 0;
-    bool exist[1000000001] = {};
-    vector<int> a;
-    queue<int> q;
+    int n, tmp, ans = 0;
     cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> tmp;
-        a.push_back(tmp);
-        max = std::max(tmp, max);
-        exist[tmp] = true;
+    vector<int> a(n);
+    rep(i, n){
+        cin >> a[i];
     }
-    sort(a.begin(), a.end());
 
-    q.push(max/2);
-    while(tmp <= max){
-        tmp = q.front();
-        if(exist[tmp] && tmp != max) break;
-        else {
-            if(tmp == max/2) {
-                q.push(tmp + 1);
-                q.push(tmp - 1);
-            }
-            else if(tmp < max/2) q.push(tmp-1);
-            else q.push(tmp+1);
+    sort(a.begin(), a.end(), greater<int>());
+    int tar = (a[0] + 1) / 2;
+    int diff = a[0] - tar;
+    for(int n : a){
+        if(abs(tar - n) < diff){
+            diff = abs(tar - n);
+            ans = n;
         }
-        q.pop();
     }
-    cout << max << ' ' << tmp << endl;
+    cout << a[0] << " " << ans << endl;
 }
