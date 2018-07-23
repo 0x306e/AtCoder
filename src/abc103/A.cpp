14c78e27f4d9a6ec4175f59212acb8c5c88618d4
0x306e <3so.d.na@gmail.com>
2018-07-23 16:32:33 +0900

    うんこ漏らした

---
+++ src/abc103/A.cpp
@@ -0,0 +1,17 @@
+#define _overload3(_1,_2,_3,name,...) name
+#define _rep(i,n) repi(i,0,n)
+#define repi(i,a,b) for(int i=int(a);i<int(b);++i)
+#define rep(...) _overload3(__VA_ARGS__,repi,_rep,)(__VA_ARGS__)
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+
+int main(int argc, char* argv[]){
+    int a, b, c;
+    cin >> a >> b >> c;
+    int mx = max(a, max(b, c));
+    int mn = min(a, min(b, c));
+
+    cout << abs(mx-mn) << endl;
+}
+
